7347f8553b473af56981fea82a101613116fd709
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-07-29 16:05:00 +0800

    updates

--- Day2/bootstrap/resources/views/dashboard.blade.php
+++ Day2/bootstrap/resources/views/dashboard.blade.php
@@ -28,38 +28,59 @@
                     <h6 class="card-subtitle mb-2 text-muted">
                         <small>Active accounts we can see on the dashboard</small>
                     </h6>  
-                    <div class="table-responsive mt-5">
+                    <div class="table-responsivex mt-5">
                         <table class="table align-middle">
                             <thead>
                                 <tr>
                                     <td></td>
-                                    <td class="text-muted fs-6"><small>Fullname</small></td>
-                                    <td class="text-muted fs-6"><small>Email Address</small></td>
-                                    <td class="text-muted fs-6"><small>Location</small></td>
-                                    <td></td>
+                                    <td class="text-muted fs-6"><small>FULL NAME</small></td>
+                                    <td class="text-muted fs-6"><small>EMAIL ADDRESS</small></td>
+                                    <td class="text-muted fs-6"><small>LOCATION</small></td>
+                                    <td width="5%"></td>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>John Doe</td>
-                                    <td>jd2021@gmail.com</td>
-                                    <td>Philippines</td>
-                                    <td>
-                                        <div class="dropdown">
-                                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                              Dropdown button
-                                            </button>
-                                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                              <li><a class="dropdown-item" href="#">Action</a></li>
-                                              <li><a class="dropdown-item" href="#">Another action</a></li>
-                                              <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                            </ul>
-                                          </div>
-                                    </td>
-                                </tr> 
+                                @for($i=0; $i <= 5; $i++)
+                                    @php
+                                        
+                                    @endphp
+                                    <tr>
+                                        <td>{{ $i+ 1}}</td>
+                                        <td></td>
+                                        <td>jd2021@gmail.com</td>
+                                        <td>Philippines</td>
+                                        <td>
+                                            <div class="dropdown">
+                                                <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                                    Actions
+                                                </button>
+                                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                                </ul>
+                                            </div>
+                                        </td>
+                                    </tr> 
+                                @endfor
                             </tbody>
                         </table>
+
+                        <nav aria-label="...">
+                            <ul class="pagination">
+                              <li class="page-item disabled">
+                                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                              </li>
+                              <li class="page-item"><a class="page-link" href="#">1</a></li>
+                              <li class="page-item active" aria-current="page">
+                                <a class="page-link" href="#">2</a>
+                              </li>
+                              <li class="page-item"><a class="page-link" href="#">3</a></li>
+                              <li class="page-item">
+                                <a class="page-link" href="#">Next</a>
+                              </li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
             </div>
@@ -71,6 +92,16 @@
 
 
     <style>
+        .table>:not(:last-child)>:last-child>*{
+            border-bottom-color: #eaeaea;
+        }
+        .page-link{
+            color: #6d767e;
+        }
+        .page-item.active .page-link { 
+            background-color: #ffc007;
+            border-color: #ffc007;
+        }
         .nav-pills .nav-link.active, .nav-pills .show>.nav-link{
             background-color: #ffc007;
         } 
@@ -78,7 +109,7 @@
           height: 100vh;
         }
         body{
-          overflow-y: hidden;
+          overflow: hidden;
           background: #e9e9e9;
         }
         #content{ 
