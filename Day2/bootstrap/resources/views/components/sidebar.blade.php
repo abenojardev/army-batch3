67f43717881f92fb62e714db13a12f386a31b0bb
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-07-29 15:15:41 +0800

    up

--- Day2/bootstrap/resources/views/components/sidebar.blade.php
+++ Day2/bootstrap/resources/views/components/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
+<div id="sidebar" class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark">
   <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
     <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
     <span class="fs-4">Sidebar</span>
@@ -39,7 +39,7 @@
   <hr>
   <div class="dropdown">
     <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-      <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+      <img src="" alt="" width="32" height="32" class="rounded-circle me-2">
       <strong>mdo</strong>
     </a>
     <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
--- Day2/bootstrap/resources/views/dashboard.blade.php
+++ Day2/bootstrap/resources/views/dashboard.blade.php
@@ -18,11 +18,37 @@
     </div>
 
     <div class="row">
-        
+        <div class="col-3">
+            @include('components.sidebar')
+        </div>
+        <div id="content" class="col-9 p-4">
+            <ol>
+            @for($i=0; $i<=100; $i++)
+                <li>conten # {{ $i }}</li>
+            @endfor
+            </ol>
+        </div>
     </div>
 
-    @include('components.sidebar')
 
+   
+
+
+    <style>
+        #sidebar{
+          height: 100vh;
+        }
+        body{
+          overflow-y: hidden;
+        }
+        #content{ 
+          overflow-y: scroll; 
+          height: 90vh; 
+        }
+        ::-webkit-scrollbar{
+            display: none;
+        }
+    </style>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
