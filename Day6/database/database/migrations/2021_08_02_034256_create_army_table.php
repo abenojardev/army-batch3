6425cc11543db096b44ea178d75801174c2c556a
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-02 14:39:53 +0800

    up

--- Day6/database/database/migrations/2021_08_02_034256_create_army_table.php
+++ Day6/database/database/migrations/2021_08_02_034256_create_army_table.php
@@ -19,32 +19,24 @@ class CreateArmyTable extends Migration
          */
         Schema::create('army', function (Blueprint $table) {
             $table->id();
-            
             // alpha
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('middle_name');
-            
+            $table->string('middle_name'); 
             // if numeric
-            $table->integer('age');
-
+            $table->integer('age'); 
             // for dates
-            $table->date('birth_date');
-
+            $table->date('birth_date'); 
             // for preset values
             $table->enum('gender', [
                 'Male',
                 'Female'
-            ]); 
-
+            ]);  
             // for datas with decimal points
             $table->double('height');
-            $table->double('weight');
-
-
+            $table->double('weight'); 
             // for datas that could be bigger than 255 chars
-            $table->longText('address');
-
+            $table->longText('address'); 
             $table->timestamps();
         });
     }
---
+++ Day6/library/database/migrations/2021_08_02_063534_create_librarians_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrariansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('librarians', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('middle_name'); 
+            $table->enum('gender', [
+                'Male',
+                'Female'
+            ]);
+            $table->date('birth_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('librarians');
+    }
+}
