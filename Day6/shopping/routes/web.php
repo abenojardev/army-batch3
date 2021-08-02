e879b84a8dc0712fc11c2ca4244a3b679d4e424c
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-02 16:03:23 +0800

    up

--- Day6/shopping/routes/web.php
+++ Day6/shopping/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShoppingController;
 
 Route::get('/', [
-
+    ShoppingController::class,
+    'index'
 ])->name('shopping.home');
