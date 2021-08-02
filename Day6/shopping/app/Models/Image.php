ffe5bb0407f16c949cd8b822738ca1ed15a401b7
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-02 15:50:29 +0800

    up

--- Day6/shopping/app/Models/Image.php
+++ Day6/shopping/app/Models/Image.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+
+    protected $table = 'images';
+
+    protected $fillable = [
+        'product_id',
+        'image'
+    ];
 }
