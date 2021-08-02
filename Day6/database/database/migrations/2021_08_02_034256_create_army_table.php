80ca32f28ba3101f05b955dd0722ed5b0b3ea4d0
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-02 12:05:10 +0800

    up

---
+++ Day6/database/database/migrations/2021_08_02_034256_create_army_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArmyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /**
+         * no spaces, user underscore
+         * small letters
+         */
+        Schema::create('army', function (Blueprint $table) {
+            $table->id();
+            
+            // alpha
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('middle_name');
+            
+            // if numeric
+            $table->integer('age');
+
+            // for dates
+            $table->date('birth_date');
+
+            // for preset values
+            $table->enum('gender', [
+                'Male',
+                'Female'
+            ]); 
+
+            // for datas with decimal points
+            $table->double('height');
+            $table->double('weight');
+
+
+            // for datas that could be bigger than 255 chars
+            $table->longText('address');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('army');
+    }
+}
