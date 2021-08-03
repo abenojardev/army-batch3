5696315ebbf09ccc8649931e66be6fb58999f726
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-03 11:37:06 +0800

    up

--- Day7/Songs/app/Models/Artist.php
+++ Day7/Songs/app/Models/Artist.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Artist extends Model
 {
     use HasFactory;
+
+    protected $table = 'artists';
+    
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'band',
+    ];
 }
