0ab26fe3c678b70f5463e121841bf1e9c565c589
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-03 11:05:37 +0800

    up

--- Day7/relationship/database/seeders/BookSeeder.php
+++ Day7/relationship/database/seeders/BookSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Book;
 
 class BookSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class BookSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Book::create([
+            'title' => 'The Adventures of Tom Sawyer',
+            'category' => ,
+            'date_published' => ,
+            'author_id' => 0,
+            'publisher_id' => 0
+        ])
     }
 }
