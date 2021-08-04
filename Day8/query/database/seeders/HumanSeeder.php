fb576609e3f7919eb63d9313edc615718960820b
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-04 10:24:39 +0800

    up

--- Day8/query/database/seeders/HumanSeeder.php
+++ Day8/query/database/seeders/HumanSeeder.php
@@ -17,6 +17,8 @@ class HumanSeeder extends Seeder
         // 100 random
 
         for($i=0; $i <= 100; $i++){
+
+            $faker = new Faker\Factory;
             Human::create([
                 'age' => '',
                 'birth_date' => '',
