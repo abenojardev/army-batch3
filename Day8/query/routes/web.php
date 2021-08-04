80b4ded7dafa76e825e8a6d5869ed033cbc53e6c
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-04 11:05:21 +0800

    up

--- Day8/query/routes/web.php
+++ Day8/query/routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QueryController;
 
+/**
+ * $type is not required '?'
+ * http://localhost:8000/{type}
+ * http://localhost:8000/simple_query
+ */
 Route::get('/{type?}',[
     QueryController::class,
     'index'
