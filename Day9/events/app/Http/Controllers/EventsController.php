2d3e9eda11048668131bba7c4ba9c47c76a23585
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-05 12:08:26 +0800

    up

--- Day9/events/app/Http/Controllers/EventsController.php
+++ Day9/events/app/Http/Controllers/EventsController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
+use Redirect;
 
 class EventsController extends Controller
 {
-    //
+    protected $event;
+
+    public function __construct(Event $event)
+    {
+        $this->event = $event;
+    }
+
+    public function index()
+    {
+        
+    }
+    public function update()
+    {
+        
+    }
+    public function delete()
+    {
+        
+    }
+    public function create()
+    {
+        $this->event->create([ 
+            'title',
+            'location',
+            'date',
+            'description',
+            'entrance_fee',
+        ]);
+
+        return Redirect::route('indesx');
+    }
 }
