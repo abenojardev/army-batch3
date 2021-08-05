cb973a140bbf52fe90fcee7a1c0769ed658b76fb
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-05 14:59:29 +0800

    up

--- Day9/personnels/app/Http/Controllers/PersonnelController.php
+++ Day9/personnels/app/Http/Controllers/PersonnelController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Personnel;
 
 class PersonnelController extends Controller
 {
+    protected $personnel;
+
+    public function __construct(Personnel $personnel)
+    {
+        $this->personnel = $personnel;
+    }
+    
     public function index()
     {
         return view('index');
