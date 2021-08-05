2822132f470f6546e8c91fd32e0c3f7c538ff155
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-05 14:16:45 +0800

    up

--- Day9/personnels/app/Models/Personnel.php
+++ Day9/personnels/app/Models/Personnel.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Personnel extends Model
 {
     use HasFactory;
+
+    protected $table = 'personnels';
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'division',
+        'rank',
+        'badge_number'
+    ];
 }
