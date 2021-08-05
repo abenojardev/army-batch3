be3dad656f9527db4bb1bd6f4b4bc2489842020d
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-05 10:42:15 +0800

    up

--- Day9/transactions/app/Http/Controllers/TransactionController.php
+++ Day9/transactions/app/Http/Controllers/TransactionController.php
@@ -3,26 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class TransactionController extends Controller
 {
     public function view()
     {
-
     }
 
     public function create()
     {
-
     }
 
     public function update()
     {
-
     }
 
     public function delete()
     {
-
     }
 }
