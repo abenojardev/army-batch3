45d566b4310f84e2747cc5102ba3277c225707e5
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-05 14:42:46 +0800

    up

--- Day9/personnels/resources/views/index.blade.php
+++ Day9/personnels/resources/views/index.blade.php
@@ -10,7 +10,7 @@
 </head>
 <body class="p-5">
     
-    <a class="btn btn-success">Create Personnel</a>
+    <a href="" class="btn btn-success">Create Personnel</a>
 
     <table class="table mt-5">
         <thead>
@@ -27,18 +27,7 @@
             <td>Mark</td>
             <td>Otto</td>
             <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          </tr>  
         </tbody>
       </table>
 
