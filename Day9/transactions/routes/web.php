f852aa5f0f3b134d029839f794e5b9130ff1f902
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-05 10:10:34 +0800

    up

--- Day9/transactions/routes/web.php
+++ Day9/transactions/routes/web.php
@@ -7,3 +7,18 @@ Route::get('/',[
     TransactionController::class,
     'view'
 ])->name('view');
+
+Route::get('/create',[
+    TransactionController::class,
+    'create'
+])->name('create');
+
+Route::get('/update',[
+    TransactionController::class,
+    'update'
+])->name('update');
+
+Route::get('/delete',[
+    TransactionController::class,
+    'delete'
+])->name('delete');
