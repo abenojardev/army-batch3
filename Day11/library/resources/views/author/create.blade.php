13c1cf944f6bd8553350efd35e503d1ced5bec05
xsodia <cressebatan08@gmail.com>
2021-09-06 11:17:11 +0800

    up

---
+++ Day11/library/resources/views/author/create.blade.php
@@ -0,0 +1,10 @@
+@extends('main-layout')
+@section('content')
+    <div class="container">
+        <div class="row pt-5">
+            <div class="col-12">
+                 
+            </div>
+        </div>
+    </div>
+@endsection
