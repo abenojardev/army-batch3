490cd2b45a598102115346059ad249b458ad58d0
xsodia <cressebatan08@gmail.com>
2021-09-06 11:28:35 +0800

    up

---
+++ Day11/library/resources/views/category/index.blade.php
@@ -0,0 +1,32 @@
+@extends('main-layout')
+@section('content')
+    <div class="container">
+        <div class="row pt-5">
+            <div class="col-12">
+                
+                <a href="{{ URL::route('category.create') }}" class="btn btn-dark">
+                    Add New Category
+                </a>
+                
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td scope="row">1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr> 
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
