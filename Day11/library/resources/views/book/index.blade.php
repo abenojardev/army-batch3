390061fc5a315b2ee0121fd33cc3583fce3fd53c
xsodia <cressebatan08@gmail.com>
2021-09-06 10:45:08 +0800

    up

--- Day11/library/resources/views/book/index.blade.php
+++ Day11/library/resources/views/book/index.blade.php
@@ -1,1 +1,6 @@
 @extends('main-layout')
+@section('content')
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam temporibus esse enim dolor voluptatem, architecto tenetur non cupiditate perspiciatis, vero aspernatur velit praesentium. Facere laboriosam quia id maxime consequatur qui!
+    </p>
+@endsection
