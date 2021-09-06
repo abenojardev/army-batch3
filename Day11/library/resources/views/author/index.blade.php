c7b9bc4ec61dbcbaff8baec741f32b7e5887107f
xsodia <cressebatan08@gmail.com>
2021-09-06 11:08:54 +0800

    up

--- Day11/library/resources/views/author/index.blade.php
+++ Day11/library/resources/views/author/index.blade.php
@@ -1,4 +1,27 @@
 @extends('main-layout')
 @section('content')
-
+    <div class="container">
+        <div class="row pt-5">
+            <div class="col-12">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td scope="row">1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr> 
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
