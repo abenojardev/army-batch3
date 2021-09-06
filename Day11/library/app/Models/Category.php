9e7886ecbf28d0b142a26b06501dd7c9a94b8473
xsodia <cressebatan08@gmail.com>
2021-09-06 10:04:39 +0800

    up

--- Day11/library/app/Models/Category.php
+++ Day11/library/app/Models/Category.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+ 
+    protected $table = 'category';
+
+    protected $fillable = [
+        'name'
+    ];
 }
