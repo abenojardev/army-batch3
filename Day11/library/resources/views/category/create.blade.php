3abcf3fc095874e22bfe5374ed7cd8e4898ecc63
xsodia <cressebatan08@gmail.com>
2021-09-06 11:34:47 +0800

    up

--- Day11/library/resources/views/category/create.blade.php
+++ Day11/library/resources/views/category/create.blade.php
@@ -4,12 +4,11 @@
         <div class="row pt-5">
             <div class="col-12">
                 <form>
-                    <div class="mb-3">
-                      <label class="form-label">Email address</label>
-                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                    </div> 
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                  <div class="mb-3">
+                    <label class="form-label">Email address</label>
+                    <input type="email" class="form-control">
+                  </div> 
+                  <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
         </div>
