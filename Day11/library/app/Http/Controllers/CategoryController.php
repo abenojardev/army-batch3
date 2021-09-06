7bc61b4c9029d396f7af93acda39003c1527834c
xsodia <cressebatan08@gmail.com>
2021-09-06 11:24:26 +0800

    up

--- Day11/library/app/Http/Controllers/CategoryController.php
+++ Day11/library/app/Http/Controllers/CategoryController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('category.index');
+    }
+    
+    public function create()
+    {
+        return view('category.create');
+    }
 }
