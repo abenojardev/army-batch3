9ddf6c0f8429df60c2670174e847235e1b3174bb
xsodia <cressebatan08@gmail.com>
2021-09-06 10:56:30 +0800

    up

--- Day11/library/resources/views/book/index.blade.php
+++ Day11/library/resources/views/book/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <div class="row pt-5">
-            <div class="col-3">
+            <div class="col-3 mb-2">
                 <div class="card">
                     <img src="..." class="card-img-top" alt="...">
                     <div class="card-body">
