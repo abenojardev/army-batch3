04c06069f22f8b68678b0a62c96879ef53b6922b
xsodia <cressebatan08@gmail.com>
2021-09-06 10:51:20 +0800

    up

--- Day11/library/resources/views/book/index.blade.php
+++ Day11/library/resources/views/book/index.blade.php
@@ -1,6 +1,17 @@
 @extends('main-layout')
 @section('content')
-    <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam temporibus esse enim dolor voluptatem, architecto tenetur non cupiditate perspiciatis, vero aspernatur velit praesentium. Facere laboriosam quia id maxime consequatur qui!
-    </p>
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </div>
 @endsection
