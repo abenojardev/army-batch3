b4cb85e7a85091293cb3cdad025c7c3d95120ec2
xsodia <cressebatan08@gmail.com>
2021-09-06 11:54:26 +0800

    up

--- Day11/library/app/Http/Controllers/CategoryController.php
+++ Day11/library/app/Http/Controllers/CategoryController.php
@@ -10,7 +10,7 @@ class CategoryController extends Controller
 
     public function __construct(Request $request)
     {
-        $request = $request;
+        $this->request = $request;
     }
 
     public function index()
