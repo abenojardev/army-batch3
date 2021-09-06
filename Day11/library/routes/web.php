7bfe23b88a519298826dcbf177912807a8fff91d
xsodia <cressebatan08@gmail.com>
2021-09-06 10:17:08 +0800

    up

--- Day11/library/routes/web.php
+++ Day11/library/routes/web.php
@@ -4,4 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\CategoryController;
- 
+
+
+Route::get('/', [
+    BookController::class,
+    'index'
+]);
