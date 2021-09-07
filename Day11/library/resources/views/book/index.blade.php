6f342432296f40b814af4c04f704a1fc57022177
xsodia <cressebatan08@gmail.com>
2021-09-07 12:27:58 +0800

    up

--- Day11/library/resources/views/book/index.blade.php
+++ Day11/library/resources/views/book/index.blade.php
@@ -8,22 +8,24 @@
             </div>
             <div class="col-9"></div>
 
-            <div class="col-3 mb-2">
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">
-                            Some quick example text to build 
-                            on the card title and make up the 
-                            bulk of the card's content.
-                        </p>
-                        <a href="#" class="btn btn-primary">
-                            Go somewhere
-                        </a>
+            @foreach($books as $book)
+                <div class="col-3 mb-2">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">
+                                Some quick example text to build 
+                                on the card title and make up the 
+                                bulk of the card's content.
+                            </p>
+                            <a href="#" class="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
                     </div>
-                </div>
-            </div> 
+                </div> 
+            @endforeach
         </div>
     </div>
 @endsection
