84acc0d5171ea51ea2edbf79f589c36eae4ee4b4
xsodia <cressebatan08@gmail.com>
2021-09-07 14:22:18 +0800

    up

--- Day12/auth/database/seeders/UserSeeder.php
+++ Day12/auth/database/seeders/UserSeeder.php
@@ -14,6 +14,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'johndoe@gmail.com',
+            'password' => bcrypt('1234')
+        ]);
     }
 }
