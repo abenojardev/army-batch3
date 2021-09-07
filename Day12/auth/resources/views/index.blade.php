0b1bae87eabfb3ef4982597b06d0cd6db6e8b636
xsodia <cressebatan08@gmail.com>
2021-09-07 14:52:24 +0800

    up

--- Day12/auth/resources/views/index.blade.php
+++ Day12/auth/resources/views/index.blade.php
@@ -14,14 +14,15 @@
         <div class="row d-flex justify-content-center pt-5">
             <div class="col-6 bg-warning p-5">
                 <form>
+                    @csrf
                     <h3 class="text-dark">Log In</h3>
                     <div class="mb-3">
                         <label class="form-label">Email address</label>
-                        <input type="email" class="form-control" > 
+                        <input type="email" name="email" class="form-control" > 
                     </div>
                     <div class="mb-3">
                         <label class="form-label">Password</label>
-                        <input type="password" class="form-control">
+                        <input type="password" name="password" class="form-control">
                     </div> 
                     <button type="submit" class="btn btn-dark">Log In</button>
                 </form>
