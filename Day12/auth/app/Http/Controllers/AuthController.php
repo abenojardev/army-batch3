777fec3a78ccc0d01d7fe72486247fc76c269f53
xsodia <cressebatan08@gmail.com>
2021-09-07 14:56:33 +0800

    up

--- Day12/auth/app/Http/Controllers/AuthController.php
+++ Day12/auth/app/Http/Controllers/AuthController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth, Redirect;
 
 class AuthController extends Controller
 {
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+    
     public function index()
     {
         return view('index');
@@ -14,5 +22,10 @@ class AuthController extends Controller
     public function home()
     {
         return view('home');
+    }
+
+    public function verify()
+    {
+
     }
 }
