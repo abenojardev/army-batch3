710ddb4ab566da9884c507c2566f36d228ea29c8
xsodia <cressebatan08@gmail.com>
2021-09-09 10:53:03 +0800

    up

--- Day14/ecom/app/Http/Controllers/Admin/ProductsController.php
+++ Day14/ecom/app/Http/Controllers/Admin/ProductsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Redirect, Storage;
 
 class ProductsController extends Controller
 {
@@ -27,6 +28,27 @@ class ProductsController extends Controller
 
     public function create_save()
     {
-        
+        $img_name = strtotime(date('Y-m-d'))
+            .'_'.rand(11111,99999)
+            .'.'.$this->request->img->extension();
+
+        /**
+         * putFileAs(path, image object, image name);
+         */
+        Storage::disk('public')->putFileAs(
+            'products',
+            $this->request->img,
+            $img_name
+        );
+
+        $this->request->merge([
+            'image' => 'products/'.$img_name
+        ]);
+
+        Product::create(
+            $this->request->except('_token')
+        );
+
+        return Redirect::route('admin.products');
     }
 }
