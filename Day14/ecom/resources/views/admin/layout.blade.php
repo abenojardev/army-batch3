1f707692170dc77ebed0bcf295986155423bdd07
xsodia <cressebatan08@gmail.com>
2021-09-09 10:33:10 +0800

    up

--- Day14/ecom/resources/views/admin/layout.blade.php
+++ Day14/ecom/resources/views/admin/layout.blade.php
@@ -48,50 +48,7 @@
         </div>
     </nav>
     
-    <div class="content-wrapper">
-        <div class="container-fluid">
-            <!-- Breadcrumbs-->
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item">
-                    <a href="#">Dashboard</a>
-                </li>
-                <li class="breadcrumb-item active">Tables</li>
-            </ol>
-            <!-- Example DataTables Card-->
-            <div class="card mb-3">
-                <div class="card-header">
-                    <i class="fa fa-table"></i> Data Table Example</div>
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Position</th>
-                                    <th>Office</th>
-                                    <th>Age</th>
-                                    <th>Start date</th>
-                                    <th>Price</th>
-                                </tr>
-                            </thead> 
-                            <tbody>
-                                <tr>
-                                    <td>Tiger Nixon</td>
-                                    <td>System Architect</td>
-                                    <td>Edinburgh</td>
-                                    <td>61</td>
-                                    <td>2011/04/25</td>
-                                    <td>$320,800</td>
-                                </tr> 
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <!-- /tables-->
-        </div>
-        <!-- /container-fluid-->
-    </div> 
+    @yield('content')
     
     <!-- Bootstrap core JavaScript-->
     <script src="{{ URL::asset('theme/admin/vendor/jquery/jquery.min.js') }}"></script>
