d3617b0c422e706f200683c0e4390ad28b059e30
xsodia <cressebatan08@gmail.com>
2021-09-09 14:26:46 +0800

    up

--- Day14/ecom/app/Http/Controllers/Site/AuthController.php
+++ Day14/ecom/app/Http/Controllers/Site/AuthController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
 use App\Models\User;
-use Redirect;
+use Redirect, Auth;
 
 class AuthController extends Controller
 {
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+    
     public function index()
     {
         return view('site.auth.login');
