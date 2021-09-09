9d28fcb1a3c9c83ef90f12126dd3193e241e0e61
xsodia <cressebatan08@gmail.com>
2021-09-09 15:16:49 +0800

    up

--- Day14/ecom/routes/site/listings.php
+++ Day14/ecom/routes/site/listings.php
@@ -6,3 +6,8 @@ Route::get('/', [
     ListingsController::class,
     'index'
 ])->name('homepage');
+
+Route::post('/add-to-cart', [
+    ListingsController::class,
+    'add_to_cart'
+])->name('add.to.cart');
