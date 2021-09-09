76c142c8b0024ce4e26d710d15215244e539ad51
xsodia <cressebatan08@gmail.com>
2021-09-09 10:32:08 +0800

    up

--- Day14/ecom/resources/views/admin/products/index.blade.php
+++ Day14/ecom/resources/views/admin/products/index.blade.php
@@ -16,7 +16,9 @@
 
 <body class="fixed-nav sticky-footer" id="page-top">
     <nav class="navbar navbar-expand-lg navbar-dark bg-default fixed-top" id="mainNav">
-        <a class="navbar-brand" href="index.html"><img src="img/logo.svg" alt="" width="142" height="36"></a>
+        <a class="navbar-brand">
+            <img src="{{ URL::asset('theme/img/logo.svg') }}" alt="" width="142" height="36">
+        </a> 
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -27,92 +29,7 @@
                         <i class="fa fa-fw fa-dashboard"></i>
                         <span class="nav-link-text">Dashboard</span>
                     </a>
-                </li>
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Messages">
-                    <a class="nav-link" href="messages.html">
-                        <i class="fa fa-fw fa-envelope-open"></i>
-                        <span class="nav-link-text">Messages</span>
-                    </a>
-                </li>
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="" data-original-title="Bookings">
-                    <a class="nav-link" href="bookings.html">
-                        <i class="fa fa-fw fa-calendar-check-o"></i>
-                        <span class="nav-link-text">Bookings <span class="badge badge-pill badge-primary">6 New</span></span>
-                    </a>
-                </li>
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="My listings">
-                    <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMylistings">
-                        <i class="fa fa-fw fa-list"></i>
-                        <span class="nav-link-text">My listings</span>
-                    </a>
-                    <ul class="sidenav-second-level collapse" id="collapseMylistings">
-                        <li>
-                            <a href="listings.html">Pending <span class="badge badge-pill badge-primary">6</span></a>
-                        </li>
-                        <li>
-                            <a href="listings.html">Active <span class="badge badge-pill badge-success">6</span></a>
-                        </li>
-                        <li>
-                            <a href="listings.html">Expired <span class="badge badge-pill badge-danger">6</span></a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Reviews">
-                    <a class="nav-link" href="reviews.html">
-                        <i class="fa fa-fw fa-star"></i>
-                        <span class="nav-link-text">Reviews</span>
-                    </a>
-                </li>
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Bookmarks">
-                    <a class="nav-link" href="bookmarks.html">
-                        <i class="fa fa-fw fa-heart"></i>
-                        <span class="nav-link-text">Bookmarks</span>
-                    </a>
-                </li>
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Add listing">
-                    <a class="nav-link" href="add-listing.html">
-                        <i class="fa fa-fw fa-plus-circle"></i>
-                        <span class="nav-link-text">Add listing</span>
-                    </a>
-                </li>
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Add listing + Menu List">
-                    <a class="nav-link" href="add-listing-with-menu-list.html">
-                        <i class="fa fa-fw fa-plus-circle"></i>
-                        <span class="nav-link-text">Add listing + Menu List</span>
-                    </a>
-                </li>
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Orders Page">
-                    <a class="nav-link" href="orders.html">
-                        <i class="fa fa-fw fa-shopping-basket"></i>
-                        <span class="nav-link-text">Orders Page</span>
-                    </a>
-                </li>
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Edit Order">
-                    <a class="nav-link" href="orders.html">
-                        <i class="fa fa-fw fa-pencil"></i>
-                        <span class="nav-link-text">Edit Order</span>
-                    </a>
-                </li>
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="My profile">
-                    <a class="nav-link" href="user-profile.html">
-                        <i class="fa fa-fw fa-user"></i>
-                        <span class="nav-link-text">My Profile</span>
-                    </a>
-                </li>
-                <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
-                    <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents">
-                        <i class="fa fa-fw fa-gear"></i>
-                        <span class="nav-link-text">Components</span>
-                    </a>
-                    <ul class="sidenav-second-level collapse" id="collapseComponents">
-                        <li>
-                            <a href="charts.html">Charts</a>
-                        </li>
-                        <li>
-                            <a href="tables.html">Tables</a>
-                        </li>
-                    </ul>
-                </li>
+                </li>  
             </ul>
             <ul class="navbar-nav sidenav-toggler">
                 <li class="nav-item">
@@ -130,7 +47,7 @@
             </ul>
         </div>
     </nav>
-    <!-- /Navigation-->
+    
     <div class="content-wrapper">
         <div class="container-fluid">
             <!-- Breadcrumbs-->
