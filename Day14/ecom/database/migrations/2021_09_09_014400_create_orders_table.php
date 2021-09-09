2f0ccd323e6aab1dcbe5b53c69fdc9a613eb4235
xsodia <cressebatan08@gmail.com>
2021-09-09 09:50:26 +0800

    up

--- Day14/ecom/database/migrations/2021_09_09_014400_create_orders_table.php
+++ Day14/ecom/database/migrations/2021_09_09_014400_create_orders_table.php
@@ -12,9 +12,16 @@ class CreateOrdersTable extends Migration
      * @return void
      */
     public function up()
-    {
+    { 
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->string('reference_no');
+            $table->longText('order_items');
+            $table->double('sub_total');
+            $table->double('delivery_fee');
+            $table->double('total');
+            $table->string('status');
             $table->timestamps();
         });
     }
