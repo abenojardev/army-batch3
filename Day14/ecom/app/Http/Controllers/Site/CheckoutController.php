34375e3eaf098ab9a77e31f6d39e27dc46656b39
xsodia <cressebatan08@gmail.com>
2021-09-09 15:57:22 +0800

    up

--- Day14/ecom/app/Http/Controllers/Site/CheckoutController.php
+++ Day14/ecom/app/Http/Controllers/Site/CheckoutController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Site;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Cache;
+use App\Models\Product;
 
 class CheckoutController extends Controller
 {
@@ -14,9 +15,13 @@ class CheckoutController extends Controller
         $items = [];
 
         foreach($cart as $product_id => $item){
+            $product = Product::find($product_id);
 
+            $product->qty = $item->qty;
+
+            array_push($items, $product);
         }
-        
+
         dd($items);
         
         return view('site.checkout.cart');
