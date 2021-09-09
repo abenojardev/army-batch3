cd6b15a92b8e8842ec828b4455279338227812ff
xsodia <cressebatan08@gmail.com>
2021-09-09 15:44:55 +0800

    up

--- Day14/ecom/app/Http/Controllers/Site/ListingsController.php
+++ Day14/ecom/app/Http/Controllers/Site/ListingsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Site;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
-use Cache;
+use Cache, Redirect;
 
 class ListingsController extends Controller
 {
