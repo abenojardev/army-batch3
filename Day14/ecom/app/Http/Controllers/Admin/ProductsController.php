37db5737e030b6cae80c5e8fff243dca1d104648
xsodia <cressebatan08@gmail.com>
2021-09-09 11:31:04 +0800

    up

--- Day14/ecom/app/Http/Controllers/Admin/ProductsController.php
+++ Day14/ecom/app/Http/Controllers/Admin/ProductsController.php
@@ -28,6 +28,48 @@ class ProductsController extends Controller
         return view('admin.products.create');
     }
 
+    public function update($id)
+    {
+        return view('admin.products.update')->with([
+            'products' => Product::find($id)
+        ]);
+    }
+
+    public function update_save($id)
+    {
+        if($this->request->has('img')){ 
+            $img_name = strtotime(date('Y-m-d'))
+                .'_'.rand(11111,99999)
+                .'.'.$this->request->img->extension();
+
+            /**
+             * putFileAs(path, image object, image name);
+             */
+            Storage::disk('public')->putFileAs(
+                'products',
+                $this->request->img,
+                $img_name
+            );
+
+            $this->request->merge([
+                'image' => 'products/'.$img_name
+            ]);
+        }
+
+        Product::find($id)->update(
+            $this->request->except('_token')
+        );
+
+        return Redirect::route('admin.products');
+    }
+
+    public function delete($id)
+    {
+        Product::find($id)->delete();
+
+        return Redirect::route('admin.products');
+    }
+
     public function create_save()
     {
         $img_name = strtotime(date('Y-m-d'))
