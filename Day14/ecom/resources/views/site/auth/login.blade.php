19109bf41d428fb51bbd05b8c7b30d3ff9e83418
xsodia <cressebatan08@gmail.com>
2021-09-09 14:56:55 +0800

    up

--- Day14/ecom/resources/views/site/auth/login.blade.php
+++ Day14/ecom/resources/views/site/auth/login.blade.php
@@ -28,27 +28,11 @@
                                     <label>Password</label>
                                     <input type="password" class="form-control" name="password" id="password"> 
                                 </div>
-                                <div class="clearfix add_bottom_15">
-                                    <div class="checkboxes float-left">
-                                        <label class="container_check">Remember me
-                                          <input type="checkbox">
-                                          <span class="checkmark"></span>
-                                        </label>
-                                    </div> 
-                                </div>
+                                
                                 <div class="text-center">
                                     <input type="submit" value="Log In" class="btn_1 full-width mb_5">
                                     Don’t have an account? <a href="{{ URL::route('register') }}">Sign up</a>
-                                </div>
-                                <div id="forgot_pw">
-                                    <div class="form-group">
-                                        <label>Please confirm login email below</label>
-                                        <input type="email" class="form-control" name="email_forgot" id="email_forgot">
-                                        <i class="icon_mail_alt"></i>
-                                    </div>
-                                    <p>You will receive an email containing a link allowing you to reset your password to a new preferred one.</p>
-                                    <div class="text-center"><input type="submit" value="Reset Password" class="btn_1"></div>
-                                </div>
+                                </div> 
                             </div>
                         </form> 
                     </div>
