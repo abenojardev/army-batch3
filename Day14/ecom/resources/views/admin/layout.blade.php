0b67a632353d96c4bcc05ca2e4fde197e909cfa9
xsodia <cressebatan08@gmail.com>
2021-09-09 10:42:41 +0800

    up

--- Day14/ecom/resources/views/admin/layout.blade.php
+++ Day14/ecom/resources/views/admin/layout.blade.php
@@ -25,9 +25,9 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
                 <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-                    <a class="nav-link" href="index.html">
-                        <i class="fa fa-fw fa-dashboard"></i>
-                        <span class="nav-link-text">Dashboard</span>
+                    <a class="nav-link" href="{{ URL::route('admin.products') }}">
+                        <i class="fa fa-fw fa-list"></i>
+                        <span class="nav-link-text">Products</span>
                     </a>
                 </li>  
             </ul>
