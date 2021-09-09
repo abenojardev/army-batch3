ad8fe70bbc7db6d2e137f5f742bc24d1e7481759
xsodia <cressebatan08@gmail.com>
2021-09-09 10:30:03 +0800

    up

--- Day14/ecom/resources/views/admin/products/index.blade.php
+++ Day14/ecom/resources/views/admin/products/index.blade.php
@@ -5,26 +5,13 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Ansonika">
-    <title>FOOGRA - Admin dashboard</title>
-    <!-- Favicons-->
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
-    <!-- Bootstrap core CSS-->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Main styles -->
-    <link href="css/admin.css" rel="stylesheet">
-    <!-- Icon fonts-->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <!-- Plugin styles -->
-    <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
-    <!-- Your custom styles -->
-    <link href="css/custom.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
+    <title>Admin dashboard</title> 
+    <link href="{{ URL::asset('theme/admin/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet"> 
+    <link href="{{ URL::asset('theme/admin/css/admin.css') }}" rel="stylesheet"> 
+    <link href="{{ URL::asset('theme/admin/vendor/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css"> 
+    <link href="{{ URL::asset('theme/admin/vendor/datatables/dataTables.bootstrap4.css') }}" rel="stylesheet"> 
+    <link href="{{ URL::asset('theme/admin/css/custom.css') }}" rel="stylesheet">
 </head>
 
 <body class="fixed-nav sticky-footer" id="page-top">
@@ -767,19 +754,19 @@
     </div>
     
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ URL::asset('theme/admin/vendor/jquery/jquery.min.js"></script>
-    <script src="{{ URL::asset('theme/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ URL::asset('theme/admin/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ URL::asset('theme/admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <!-- Core plugin JavaScript-->
-    <script src="{{ URL::asset('theme/admin/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="{{ URL::asset('theme/admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
     <!-- Page level plugin JavaScript-->
-    <script src="{{ URL::asset('theme/admin/vendor/chart.js/Chart.min.js"></script>
-    <script src="{{ URL::asset('theme/admin/vendor/datatables/jquery.dataTables.js"></script>
-    <script src="{{ URL::asset('theme/admin/vendor/datatables/dataTables.bootstrap4.js"></script>
-    <script src="{{ URL::asset('theme/admin/vendor/jquery.magnific-popup.min.js"></script>
+    <script src="{{ URL::asset('theme/admin/vendor/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ URL::asset('theme/admin/vendor/datatables/jquery.dataTables.js') }}"></script>
+    <script src="{{ URL::asset('theme/admin/vendor/datatables/dataTables.bootstrap4.js') }}"></script>
+    <script src="{{ URL::asset('theme/admin/vendor/jquery.magnific-popup.min.js') }}"></script>
     <!-- Custom scripts for all pages-->
-    <script src="{{ URL::asset('theme/admin/js/admin.js"></script>
+    <script src="{{ URL::asset('theme/admin/js/admin.js') }}"></script>
     <!-- Custom scripts for this page-->
-    <script src="{{ URL::asset('theme/admin/js/admin-datatables.js"></script>
+    <script src="{{ URL::asset('theme/admin/js/admin-datatables.js') }}"></script>
 </body>
 
 </html>
