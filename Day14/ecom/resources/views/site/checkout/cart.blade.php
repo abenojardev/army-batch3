225f13f89132235ad4f283ff433a78c5b148d4b0
xsodia <cressebatan08@gmail.com>
2021-09-10 09:49:50 +0800

    up

--- Day14/ecom/resources/views/site/checkout/cart.blade.php
+++ Day14/ecom/resources/views/site/checkout/cart.blade.php
@@ -5,85 +5,86 @@
         <div class="container margin_60_40">
             <div class="row justify-content-center">
                 <div class="col-xl-6 col-lg-8" style="background: white; padding:2%;">
-                    <div class="box_booking_2 style_2">
-                        <div class="head">
-                            <div class="title">
-                                <h3>Personal Details</h3>
-                            </div>
-                        </div>
-                        <!-- /head -->
-                        <div class="main">
-                            <div class="form-group">
-                                <label>First and Last Name</label>
-                                <input class="form-control" value="{{ Auth::user()->name }}" readonly>
-                            </div> 
-                            <div class="form-group">
-                                <label>Full Address</label>
-                                <input class="form-control" name="address">
-                            </div> 
-                        </div>
-                    </div>
-                    <!-- /box_booking -->
-                    <div class="box_booking_2 style_2">
-                        <div class="head">
-                            <div class="title">
-                                <h3>Payment Method</h3>
-                            </div>
-                        </div>
-                        <!-- /head -->
-                        <div class="main">
-                            <div class="payment_select">
-                                <label class="container_radio">Credit Card
-                                    <input type="radio" value="" checked name="payment_method">
-                                    <span class="checkmark"></span>
-                                </label>
-                                <i class="icon_creditcard"></i>
-                            </div>
-                            <div class="form-group">
-                                <label>Name on card</label>
-                                <input type="text" class="form-control" id="name_card_order" name="name_card_order" placeholder="First and last name">
-                            </div>
-                            <div class="form-group">
-                                <label>Card number</label>
-                                <input type="text" id="card_number" name="card_number" class="form-control" placeholder="Card number">
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <label>Expiration date</label>
-                                    <div class="row">
-                                        <div class="col-md-6 col-6">
-                                            <div class="form-group">
-                                                <input type="text" id="expire_month" name="expire_month" class="form-control" placeholder="mm">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6 col-6">
-                                            <div class="form-group">
-                                                <input type="text" id="expire_year" name="expire_year" class="form-control" placeholder="yyyy">
-                                            </div>
-                                        </div>
-                                    </div>
+                    <form action="">
+                        <div class="box_booking_2 style_2">
+                            <div class="head">
+                                <div class="title">
+                                    <h3>Personal Details</h3>
                                 </div>
-                                <div class="col-md-6 col-sm-12">
-                                    <div class="form-group">
-                                        <label>Security code</label>
+                            </div>
+                            <!-- /head -->
+                            <div class="main">
+                                <div class="form-group">
+                                    <label>First and Last Name</label>
+                                    <input class="form-control" value="{{ Auth::user()->name }}" readonly>
+                                </div> 
+                                <div class="form-group">
+                                    <label>Full Address</label>
+                                    <input class="form-control" name="address">
+                                </div> 
+                            </div>
+                        </div> 
+                        <div class="box_booking_2 style_2">
+                            <div class="head">
+                                <div class="title">
+                                    <h3>Payment Method</h3>
+                                </div>
+                            </div>
+                            <!-- /head -->
+                            <div class="main">
+                                <div class="payment_select">
+                                    <label class="container_radio">Credit Card
+                                        <input type="radio" value="" checked name="payment_method">
+                                        <span class="checkmark"></span>
+                                    </label>
+                                    <i class="icon_creditcard"></i>
+                                </div>
+                                <div class="form-group">
+                                    <label>Name on card</label>
+                                    <input type="text" class="form-control" id="name_card_order" name="name_card_order" placeholder="First and last name">
+                                </div>
+                                <div class="form-group">
+                                    <label>Card number</label>
+                                    <input type="text" id="card_number" name="card_number" class="form-control" placeholder="Card number">
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <label>Expiration date</label>
                                         <div class="row">
-                                            <div class="col-md-4 col-6">
+                                            <div class="col-md-6 col-6">
                                                 <div class="form-group">
-                                                    <input type="text" id="ccv" name="ccv" class="form-control" placeholder="CCV">
+                                                    <input type="text" id="expire_month" name="expire_month" class="form-control" placeholder="mm">
                                                 </div>
                                             </div>
-                                            <div class="col-md-8 col-6">
-                                                <img src="{{ URL::asset('theme/img/icon_ccv.gif') }}" width="50" height="29" alt="ccv"><small>Last 3 digits</small>
+                                            <div class="col-md-6 col-6">
+                                                <div class="form-group">
+                                                    <input type="text" id="expire_year" name="expire_year" class="form-control" placeholder="yyyy">
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="col-12"> 
-                                    <button href="" class="btn_1 full-width mb_5">Place Order</button> 
-                                </div>
-                            </div>  
-                        </div>
-                    </div> 
+                                    <div class="col-md-6 col-sm-12">
+                                        <div class="form-group">
+                                            <label>Security code</label>
+                                            <div class="row">
+                                                <div class="col-md-4 col-6">
+                                                    <div class="form-group">
+                                                        <input type="text" id="ccv" name="ccv" class="form-control" placeholder="CCV">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-8 col-6">
+                                                    <img src="{{ URL::asset('theme/img/icon_ccv.gif') }}" width="50" height="29" alt="ccv"><small>Last 3 digits</small>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-12"> 
+                                        <button href="" class="btn_1 full-width mb_5">Place Order</button> 
+                                    </div>
+                                </div>  
+                            </div>
+                        </div> 
+                    </form>
                 </div> 
                 <div class="col-xl-4 col-lg-4" id="sidebar_fixed">
                     <div class="box_booking"   style="background: white; padding:4%;">
