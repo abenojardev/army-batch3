2220397abba5ef03c5133406123de1eaea803b81
xsodia <cressebatan08@gmail.com>
2021-09-10 09:36:24 +0800

    up

--- Day14/ecom/resources/views/site/checkout/cart.blade.php
+++ Day14/ecom/resources/views/site/checkout/cart.blade.php
@@ -39,58 +39,15 @@
                                 </label>
                                 <i class="icon_creditcard"></i>
                             </div>
-                            <div class="form-group">
-                                <label>Name on card</label>
-                                <input type="text" class="form-control" id="name_card_order" name="name_card_order" placeholder="First and last name">
-                            </div>
-                            <div class="form-group">
-                                <label>Card number</label>
-                                <input type="text" id="card_number" name="card_number" class="form-control" placeholder="Card number">
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <label>Expiration date</label>
-                                    <div class="row">
-                                        <div class="col-md-6 col-6">
-                                            <div class="form-group">
-                                                <input type="text" id="expire_month" name="expire_month" class="form-control" placeholder="mm">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6 col-6">
-                                            <div class="form-group">
-                                                <input type="text" id="expire_year" name="expire_year" class="form-control" placeholder="yyyy">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 col-sm-12">
-                                    <div class="form-group">
-                                        <label>Security code</label>
-                                        <div class="row">
-                                            <div class="col-md-4 col-6">
-                                                <div class="form-group">
-                                                    <input type="text" id="ccv" name="ccv" class="form-control" placeholder="CCV">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-8 col-6">
-                                                <img src="{{ URL::asset('theme/img/icon_ccv.gif') }}" width="50" height="29" alt="ccv"><small>Last 3 digits</small>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>  
-                        </div>
-                        <!-- Mount the instance within a <label> -->
-<label>Card
-    <div id="card-element"></div>
-  </label>
-  
-  <!--
-    Or create a <label> with a 'for' attribute,
-    referencing the ID of your container.
-  -->
-  <label for="card-element">Card</label>
-  <div id="card-element"></div>
+                            <div class="form-group"> 
+                                <label>
+                                    Card
+                                    <div id="card-element"></div>
+                                </label> 
+                                <label for="card-element">Card</label>
+                                <div id="card-element"></div>
+                            </div> 
+                        </div> 
                     </div> 
                 </div> 
                 <div class="col-xl-4 col-lg-4" id="sidebar_fixed">
