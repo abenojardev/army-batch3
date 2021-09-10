90774f91c602efbaf2fd7d95ed7e890cc89234a7
xsodia <cressebatan08@gmail.com>
2021-09-10 09:27:00 +0800

    up

--- Day14/ecom/app/Http/Controllers/Site/CheckoutController.php
+++ Day14/ecom/app/Http/Controllers/Site/CheckoutController.php
@@ -6,11 +6,29 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Cache;
 use App\Models\Product;
+use \Stripe\StripeClient;
 
 class CheckoutController extends Controller
 {
     public function index()
     {
+        $stripe = new StripeClient(
+            env('STRIPE_SECRET_KEY')
+          );
+
+          $stripe->checkout->sessions->create([
+            'success_url' => 'https://example.com/success',
+            'cancel_url' => 'https://example.com/cancel',
+            'payment_method_types' => ['card'],
+            'line_items' => [
+              [
+                'price' => 1000.00,
+                'quantity' => 2,
+              ],
+            ],
+            'mode' => 'payment',
+          ]);
+
         $cart = Cache::get('cart') ?? [];
         $items = [];
         $total = 0;
