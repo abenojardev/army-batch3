96296b6913fbc9b49b860fa493090df194c99dde
xsodia <cressebatan08@gmail.com>
2021-09-10 11:04:07 +0800

    up

--- Day14/ecom/resources/views/site/listing/index.blade.php
+++ Day14/ecom/resources/views/site/listing/index.blade.php
@@ -56,4 +56,18 @@
 @endsection
 @section('scripts')
     <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
+    <script>
+
+        // Enable pusher logging - don't include this in production
+        Pusher.logToConsole = true;
+    
+        var pusher = new Pusher('db5d1619725f9fe08e31', {
+          cluster: 'ap1'
+        });
+    
+        var channel = pusher.subscribe('my-channel');
+        channel.bind('my-event', function(data) {
+          alert(JSON.stringify(data));
+        });
+    </script>
 @endsection
