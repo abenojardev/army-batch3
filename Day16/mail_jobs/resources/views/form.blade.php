57b3c9e75339c18d3f8c525a07cf080b235a8b42
xsodia <cressebatan08@gmail.com>
2021-09-13 10:12:43 +0800

    up

--- Day16/mail_jobs/resources/views/form.blade.php
+++ Day16/mail_jobs/resources/views/form.blade.php
@@ -8,10 +8,9 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
-<body>
-    
+<body class="bg-dark"> 
     <div class="container">
-        <div class="row d-flex justify-content-center" height="100vh"> 
+        <div class="row d-flex justify-content-center mt-5 pt-5"> 
             <div class="col-6">
                 <form>
                     <div class="mb-3">
