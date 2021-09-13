4e52b827cda8db02bee8a6531fcd5dabe688c72e
xsodia <cressebatan08@gmail.com>
2021-09-13 10:42:51 +0800

    up

--- Day16/mail_jobs/resources/views/mail/newsletter.blade.php
+++ Day16/mail_jobs/resources/views/mail/newsletter.blade.php
@@ -15,20 +15,24 @@
                         <td>
                             <img width="100%" src="https://www.army.mil.ph/home/images/2020/FEATURED_STORIES/true-story-of-valor1.jpg" alt="">
                         </td>
+                    </tr>
+                    <tr align="center">
                         <td>
                             <h1>
                                 Outnumbered but Undaunted: A True Story of Valor
                             </h1>
                         </td>
                     </tr> 
-                    <tr>
+                    <tr align="center">
                         <td>
                             <p style="padding:5%;">
-                                Sometime in April 1990, a certain farmer and his family was harassed by a group belonging to the New People’s Army in Murcia, Negros Occidental Province. Feeling helpless about the injustice he experienced, he approached a Special Forces Company headed by a young captain and informed the soldiers about the presence of the rebels in the area. With the help of the farmer serving as guide, the company commander, Captain Arturo Ortiz immediately formulated a plan to locate the enemy.
-                                On April 6th, then Captain Ortiz led 24 soldiers and 36 Citizen Armed Force Geographical Unit (CAFGU) personnel on an exhausting 11-hour cross-country trek to conduct a raid on a massive NPA training ground. After climbing a steep cliff, Ortiz and his men were able to position themselves on a favorable location as they could fully observe more than 200 rebels holding their graduation ceremony.
-                                Relying on the element of surprise, he then directed the troops move as close as possible to the training ground’s peripheral and also reminded them to spare the civilians in the area before launching the attack. Though outnumbered by the enemy, Captain Ortiz gave the signal to strike at the enemy. Positioning himself from one team to another to coordinate the attack. During the intense firefight, he ran to the group of civilians huddled together in an effort to protect them from the hail of gunfire.
-                                When the gun battle ended after two hours, 85 rebels lay dead as their surviving comrades fled the area. The military also seized 33 high-powered-firearms, 21 rifle grenades, several hand grenades, and 5 two-way radios. Remarkably, there was no casualty on the side of the government forces or the civilians during the encounter.
-                                For his audacity, Captain Ortiz was awarded the Medal for Valor – the highest military honor for his conspicuous deed of bravery beyond the call of duty. He proved that he is a man of courage and determination in combat, displaying exceptional heroism and leadership in the finest standards of Filipino soldiery.
+                                Sometime in April 1990, a certain farmer and his family was harassed by a group belonging to the New People’s Army in Murcia, Negros Occidental Province. Feeling helpless about the injustice he experienced, he approached a Special Forces Company headed by a young captain and informed the soldiers about the presence of the rebels in the area. With the help of the farmer serving as guide, the company commander, Captain Arturo Ortiz immediately formulated a plan to locate the enemy. <br>
+                                On April 6th, then Captain Ortiz led 24 soldiers and 36 Citizen Armed Force Geographical Unit (CAFGU) personnel on an exhausting 11-hour cross-country trek to conduct a raid on a massive NPA training ground. After climbing a steep cliff, Ortiz and his men were able to position themselves on a favorable location as they could fully observe more than 200 rebels holding their graduation ceremony. <br>
+                                <br><br>
+                                Relying on the element of surprise, he then directed the troops move as close as possible to the training ground’s peripheral and also reminded them to spare the civilians in the area before launching the attack. Though outnumbered by the enemy, Captain Ortiz gave the signal to strike at the enemy. Positioning himself from one team to another to coordinate the attack. During the intense firefight, he ran to the group of civilians huddled together in an effort to protect them from the hail of gunfire. <br>
+                                When the gun battle ended after two hours, 85 rebels lay dead as their surviving comrades fled the area. The military also seized 33 high-powered-firearms, 21 rifle grenades, several hand grenades, and 5 two-way radios. Remarkably, there was no casualty on the side of the government forces or the civilians during the encounter. <br>
+                                For his audacity, Captain Ortiz was awarded the Medal for Valor – the highest military honor for his conspicuous deed of bravery beyond the call of duty. He proved that he is a man of courage and determination in combat, displaying exceptional heroism and leadership in the finest standards of Filipino soldiery. <br>
+                                <br><br>
                                 Up until today, the heroic feat of Captain Ortiz is recognized as one of the most successful military operations in the history of the Philippine Army.
                                 This act of heroism was interpreted in a movie entitled “Kagitingan: A True Story of Valor” by Vishnu Productions and is officially selected by the International Film Festival Manhattan 2020, along with films from other countries including the USA, Canada, Bulgaria, Estonia, India, Japan, Columbia, Spain, Italy, China, Russia, Austria, and Switzerland.#
                                 Watch the trailer here: https://www.youtube.com/watch?v=PStky25VCXU
