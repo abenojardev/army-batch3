765cb96b9e5483225ed7438c75d3f4eb11c2020f
xsodia <cressebatan08@gmail.com>
2021-09-14 10:18:11 +0800

    up

--- Day17/cache/app/Http/Controllers/DoctorsController.php
+++ Day17/cache/app/Http/Controllers/DoctorsController.php
@@ -15,6 +15,10 @@ class DoctorsController extends Controller
 
         $process_end = microtime(true);
         $execution_time = $process_end - $process_start;
+
+        dd(
+            'Fetching 500,000 records in '.$execution_time.'s'
+        );
     }
 
     public function with_cache()
