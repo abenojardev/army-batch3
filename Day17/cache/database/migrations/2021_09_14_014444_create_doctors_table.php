bb50d2ca455b0eb48c4e5b48f687f43a07867405
xsodia <cressebatan08@gmail.com>
2021-09-14 09:46:13 +0800

    up

--- Day17/cache/database/migrations/2021_09_14_014444_create_doctors_table.php
+++ Day17/cache/database/migrations/2021_09_14_014444_create_doctors_table.php
@@ -15,6 +15,7 @@ class CreateDoctorsTable extends Migration
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
