cc2e88e1330d3296ab3c32368fed1b7ea9eb2991
xsodia <cressebatan08@gmail.com>
2021-09-14 10:53:17 +0800

    up

--- Day17/cache/app/Http/Controllers/DoctorsController.php
+++ Day17/cache/app/Http/Controllers/DoctorsController.php
@@ -26,7 +26,19 @@ class DoctorsController extends Controller
 
     public function with_cache()
     {
-        // file driver
+        // file driver 
+        // avg 2s
+        $process_start = microtime(true);
+
+        $doctors = Doctor::all();
+
+        $process_end = microtime(true);
+        $execution_time = $process_end - $process_start;
+
+        return view('index')->with([
+            'doctors' => $doctors,
+            'execution_time' => $execution_time
+        ]);
     }
 
     // redis driver
